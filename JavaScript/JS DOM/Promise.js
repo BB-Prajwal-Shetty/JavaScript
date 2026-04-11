7d860355dad6c9341296ff35da9eaba8ce193580
Prajwal B B <prajwalbb5@gmail.com>
2026-04-11 10:34:39 +0530

    Promise method in JavaScript. Fetch product data and display in cards.
    Updated the fetch URL and modified the data handling to display product details in the DOM.

--- JavaScript/JS DOM/Promise.js
+++ JavaScript/JS DOM/Promise.js
@@ -1,9 +1,28 @@
-let url = "https://dummyjson.com/products"
+let cards = document.getElementsByClassName("cards")[0]
+console.log(cards)
+let url = "https://fakestoreapiserver.reactbd.org/api/products"
 let data = fetch(url).then((d)=>{
     return(d.json())
-}).then((d)=>{
-    let data = d.products
+}).then((fd)=>{
+    let data = fd.data
     data.map((ele)=>{
-        console.log(ele)
+        let {_id,title,image,description,category,brand,price} = ele
+        cards.innerHTML += `
+        <div class="card">
+        <br>
+        <h4>${_id}</h4>
+        <br>
+        <h1>${title}</h1>
+        <br>
+        <img src="${image}" alt="img loading...">
+        <br>
+        <p>${description}</p>
+        <br>
+        <h3>${category}</h3>
+        <br>
+        <h3>${brand}</h3>
+        <br>
+        <h4>${price}$</h4>
+        </div>`
     })
 })
